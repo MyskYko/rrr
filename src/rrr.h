03132fd9bd8ea94ad5495f70893e708cbe654b10
MyskYko <yoyuohlhjl@yahoo.co.jp>
2025-08-09 00:11:56 -0700

    add perform2

--- src/rrr.h
+++ src/rrr.h
@@ -3,6 +3,7 @@
 #include "scheduler/rrrScheduler.h"
 #include "scheduler/rrrScheduler3.h"
 #include "optimizer/rrrOptimizer.h"
+#include "optimizer/rrrOptimizer3.h"
 #include "analyzer/rrrApproxAnalyzer.h"
 #include "analyzer/rrrBddAnalyzer.h"
 #include "analyzer/rrrBddMspfAnalyzer.h"
@@ -10,6 +11,7 @@
 #include "analyzer/sat/rrrSatSolver.h"
 #include "simulator/rrrSimulator.h"
 #include "simulator/rrrSimulator2.h"
+#include "simulator/rrrSimulator3.h"
 #include "partitioner/rrrPartitioner.h"
 #include "partitioner/rrrLevelBasePartitioner.h"
 #include "extra/rrrPattern.h"
@@ -18,10 +20,8 @@ namespace rrr {
 
   template <typename Ntk, template<typename, typename, typename> typename Sch, template<typename, typename> typename Opt, template<typename> typename Par>
   void PerformInt(Ntk *pNtk, Parameter const *pPar) {
-    assert(pPar->fUseApprox + pPar->fUseBddCspf + pPar->fUseBddMspf <= 1);
-    if(pPar->fUseApprox) {
-      Scheduler<Ntk, Optimizer<Ntk, ApproxAnalyzer<Ntk, Simulator2<Ntk>>>, Partitioner<Ntk>> sch(pNtk, pPar);
-    } else if(pPar->fUseBddCspf) {
+    assert(!pPar->fUseBddCspf || !pPar->fUseBddMspf);
+    if(pPar->fUseBddCspf) {
       Sch<Ntk, Opt<Ntk, BddAnalyzer<Ntk>>, Par<Ntk>> sch(pNtk, pPar);
       sch.Run();
     } else if(pPar->fUseBddMspf) {
@@ -39,6 +39,9 @@ namespace rrr {
     if(!pPar->strPattern.empty()) {
       pPat = new Pattern;
       pPat->Read(pPar->strPattern, pNtk->GetNumPis());
+      if(!pPar->strPatternOutput.empty()) {
+        pPat->ReadOutput(pPar->strPatternOutput, pNtk->GetNumPos());
+      }
       pNtk->RegisterPattern(pPat);
     }
     Ntk *pCond = NULL;
@@ -51,7 +54,6 @@ namespace rrr {
       Gia_ManStop(pGia);
       pNtk->RegisterCond(pCond);
     }
-    assert(!pPar->fUseBddCspf || !pPar->fUseBddMspf);
     switch(pPar->nPartitionType) {
     case 0:
       PerformInt<Ntk, Scheduler, Optimizer, Partitioner>(pNtk, pPar);
@@ -59,6 +61,50 @@ namespace rrr {
     case 1:
       PerformInt<Ntk, Scheduler, Optimizer, LevelBasePartitioner>(pNtk, pPar);
       break;
+    default:
+      assert(0);
+    }
+    if(pPat) {
+      delete pPat;
+    }
+    if(pCond) {
+      delete pCond;
+    }
+  }
+
+  template <typename Ntk>
+  void Perform2(Ntk *pNtk, Parameter const *pPar) {
+    Pattern *pPat = NULL;
+    if(!pPar->strPattern.empty()) {
+      pPat = new Pattern;
+      pPat->Read(pPar->strPattern, pNtk->GetNumPis());
+      if(!pPar->strPatternOutput.empty()) {
+        pPat->ReadOutput(pPar->strPatternOutput, pNtk->GetNumPos());
+      }
+      pNtk->RegisterPattern(pPat);
+    }
+    Ntk *pCond = NULL;
+    if(!pPar->strCond.empty()) {
+      char buf[100];
+      strcpy(buf, pPar->strCond.c_str());
+      Gia_Man_t *pGia = Gia_AigerRead(buf, 0, 0, 0);
+      pCond = new Ntk;
+      pCond->Read(pGia, rrr::GiaReader<Ntk>);
+      Gia_ManStop(pGia);
+      pNtk->RegisterCond(pCond);
+    }
+    assert(!pPar->fUseBddCspf && !pPar->fUseBddMspf);
+    switch(pPar->nPartitionType) {
+    case 0: {
+      Scheduler<Ntk, Optimizer3<Ntk, ApproxAnalyzer<Ntk, Simulator3<Ntk>>>, Partitioner<Ntk>> sch(pNtk, pPar);
+      sch.Run();
+      break;
+    }
+    case 1: {
+      Scheduler<Ntk, Optimizer3<Ntk, ApproxAnalyzer<Ntk, Simulator3<Ntk>>>, LevelBasePartitioner<Ntk>> sch(pNtk, pPar);
+      sch.Run();
+      break;
+    }
     default:
       assert(0);
     }
